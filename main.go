794edb777035d064ee166dc03d753c6d5cbe3788
InfinitydeGuzma <>
2021-05-27 03:17:18 +0000

    can now tell you what price you have to pay to get into the museum

---
+++ main.go
@@ -0,0 +1,28 @@
+// Created by: Infinity de Guzman
+// Created on: May 2021
+//
+// This program shows what price you have to pay to get in the museum.
+
+package main
+
+import "fmt"
+
+func main() {
+	var userAge int
+	var userDay string
+
+	fmt.Println("Enter your age and the day of the week:")
+	fmt.Println()
+	fmt.Print("Age: ")
+	fmt.Scanln(&userAge)
+	fmt.Print("Day of the Week: ")
+	fmt.Scanln(&userDay)
+
+	if (userAge > 12 && userAge < 21) || (userDay == "Tuesday" || userDay == "Thursday") {
+		print("You are eligible for student pricing!")
+	} else if userAge < 5 || userAge > 95 {
+		print("You have free admission :)")
+	} else {
+		print("You must pay regular pricing.")
+	}
+}
